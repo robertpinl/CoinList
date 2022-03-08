617146b15a3b9cc2324dcd24c5dc0ea91713f2de
Robert Pinl <robert.pinl@icloud.com>
2022-03-08 10:06:42 -0500

    Add Loading View

---
+++ CoinList/View/ViewComponents/LoadingView.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingView.swift
+//  CoinList
+//
+//  Created by Robert P on 08.03.2022.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    
+    @State private var isLoading = false
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).opacity(0.85)
+                .ignoresSafeArea(.all)
+            Circle()
+                .stroke(Color(.secondarySystemBackground), lineWidth: 7)
+                .frame(width: 40, height: 40)
+            Circle()
+                .trim(from: 0, to: 0.3)
+                .stroke(Color.green, lineWidth: 5)
+                .frame(width: 40, height: 40)
+                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                .animation(.linear(duration: 1).repeatForever(autoreverses: false), value: isLoading)
+
+        }
+        .onAppear {
+            self.isLoading = true
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView( )
+    }
+}
